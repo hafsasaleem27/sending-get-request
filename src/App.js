9607b978b68d419698c6f1f98503fce0d0cafb5a
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-06-21 00:09:24 +0500

    fetch with async/await

--- src/App.js
+++ src/App.js
@@ -5,24 +5,42 @@ import MovieList from "./components/MovieList";
 const App = () => {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = (event) => {
-    event.preventDefault();
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovieData = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            name: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovieData);
-      })
-      .catch((err) => console.log(err));
+  const fetchMoviesHandler = async (event) => {
+    // event.preventDefault();
+
+    /* fetch with then/catch blocks */
+
+    // fetch("https://swapi.dev/api/films")
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     const transformedMovieData = data.results.map((movieData) => {
+    //       return {
+    //         id: movieData.episode_id,
+    //         name: movieData.title,
+    //         openingText: movieData.opening_crawl,
+    //         releaseDate: movieData.release_date,
+    //       };
+    //     });
+    //     setMovies(transformedMovieData);
+    //   })
+    //   .catch((err) => console.log(err));
+
+    /* fetch with async/await */
+
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+
+    const transformedMovieData = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        name: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovieData);
   };
 
   return (
